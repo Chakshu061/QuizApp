7c8d61562e0b33fa1e6203014cd8878ea52c3400
Chakshu Shaktawat <93027256+Chakshu061@users.noreply.github.com>
2022-08-22 09:37:47 +0530

    update

--- script.js
+++ script.js
@@ -45,19 +45,23 @@ const quizdata = [
 ];
 
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const submitBTN = document.getElementById("submit");
-
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizdata[currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -65,17 +69,44 @@ function loadQuiz() {
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
-    
 }
 
-submitBTN.addEventListener('click', () => {
-    currentQuiz++;
+function getSelected() {
+    let answer = undefined;
 
-    if (currentQuiz < quizdata.length) {
-        loadQuiz();
-    } else {
-        alert("You finished the quiz!")
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizdata[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizdata.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h3>You answered correctly at ${score}/${quizdata.length} questions.</h3>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
     }
-
-    loadQuiz();
-})
+});
